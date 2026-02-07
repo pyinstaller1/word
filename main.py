ca631fa3adaa22f66d9f8a7a273253de842abe05
pyinstaller1 <145110542+pyinstaller1@users.noreply.github.com>
2026-02-08 00:44:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+print(7)
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
@@ -10,3 +12,4 @@ async def read_root(request: Request):
     # 화면에 찍을 숫자 1
     result = 1
     return templates.TemplateResponse("index.html", {"request": request, "result": result})
+
